014a79e2071454d83a01936acd14467faed3b148
Simangaliso Madonsela <njabulos2001@gmail.com>
2026-02-15 20:50:02 +0200

    Enhance Swagger docs with XML comments and metadata
    Added custom API metadata and XML comments support to Swagger UI. Enabled XML documentation file generation in the project.

--- src/UserManagement.Api/Program.cs
+++ src/UserManagement.Api/Program.cs
@@ -21,7 +21,20 @@ builder.Services.AddScoped<IUserService, UserService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "User Management API",
+        Version = "v1",
+        Description = "RESTful API for managing users, groups, and permissions"
+    });
+
+    // Include XML comments in Swagger
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
